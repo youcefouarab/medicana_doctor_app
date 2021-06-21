64c5608d4bb965cb19bc699356b13d7a98d7f9fc
Youcef Ouarab <hy_ouarab@esi.dz>
2021-06-21 22:25:10 +0100

    feat: prescribe treatment interface

--- app/src/main/java/com/example/medicana/fragment/ScannerFragment.kt
+++ app/src/main/java/com/example/medicana/fragment/ScannerFragment.kt
@@ -1,13 +1,17 @@
 package com.example.medicana.fragment
 
 import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.medicana.R
+import com.example.medicana.SHARED_PREFS
+import com.example.medicana.util.navController
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_need_auth.*
 
 class ScannerFragment : Fragment() {
 
@@ -22,12 +26,22 @@ class ScannerFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scanner, container, false)
+        val connected =
+            (act.getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE))
+                .getBoolean("connected", false)
+        return if (connected) {
+            inflater.inflate(R.layout.fragment_scanner, container, false)
+        } else {
+            inflater.inflate(R.layout.layout_need_auth, container, false)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         act.nav_bottom?.visibility = View.VISIBLE
+
+        need_auth_button?.setOnClickListener {
+            navController(act).navigate(R.id.action_nav_host_to_authFragment)
+        }
     }
 }
